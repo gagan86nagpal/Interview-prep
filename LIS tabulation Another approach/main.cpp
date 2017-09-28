d817cbfb61b0dd7834200bfbe0a04a4bc91b1492
gagan86nagpal <gagannagpal68@gmail.com>
2017-09-28 15:26:26 +0530

    LIS

--- LIS tabulation Another approach/main.cpp
+++ LIS tabulation Another approach/main.cpp
@@ -5,25 +5,33 @@ int dp[1000];
 int a[1000];
 int lis(int n)
 {
-    int i,j;
-    for(i=0;i<n;i++)
-    {
-        dp[i]=1;
-        for(j=0;j<i;j++)
-        {
-            if(a[i]>a[j] && dp[j]+1 > dp[i])
-                dp[i]=dp[j]+1;
-        }
-    }
-    return dp[n-1];
+    int i,j,ma=-1;
+  for (i = 0; i < n; i++ )
+    dp[i] = 1;
+
+   for (i = 1; i < n; i++ )
+    for (j = 0; j < i; j++ )
+        if ( a[i] > a[j] && dp[i] < dp[j] + 1)
+            dp[i] = dp[j] + 1;
+
+    for (i = 0; i < n; i++ )
+        if (ma < dp[i])
+            ma = dp[i];
+    return ma;
 }
 int main()
-{ int n;
+{
+    int n;
    cin>>n;
-   a[0]=-1;
-   for(int i=1;i<=n;i++)
+   for(int i=0;i<n;i++)
     cin>>a[i];
     int ans = lis(n);
     cout<<"LIS Length:"<<ans<<"\n";
+
     return 0;
 }
+
+/*
+6
+2 5 1 5 2 1
+*/
