ac85f139ee254f2d36c8a2f833165c88e1f93559
gagan86nagpal <gagannagpal68@gmail.com>
2017-09-30 09:47:56 +0530

    COmments added

--- Word Break Problem/main.cpp
+++ Word Break Problem/main.cpp
@@ -5,15 +5,21 @@ using namespace std;
 set < string> dict;
 string to_search;
 
+
+// if we can find a prefix in dictionaty then recurse to the subproblem i.e. (string excluding prefix)
+
+// we can encounter lots of overlapping subproblems , so use dp memoization
 int dp[100000];
 void init(unsigned l)
 {
     for(unsigned i =0;i<=l;i++)
         dp[i]=-1;
 }
+//using index to keep track of the string to be searched
+// as using string and xalling subst , costs us linear time
 bool isBreakable(unsigned index=0)
 {
-    if(to_search.size()==index)
+    if(to_search.size()==index) // base case, whole string is matched
     {
         dp[index]=true;
         return true;
@@ -27,13 +33,12 @@ bool isBreakable(unsigned index=0)
             flag=flag|| dp[index+i];
         else
         {
-            cout<<index+i<<" ";
             if(dict.find( to_search.substr(index,i) )!=dict.end()) // found
                {
-                   flag=flag|| isBreakable(index+i);
+                   flag=flag|| isBreakable(index+i);  // do not call , if we have found an answer, boolean shortcut
                }
         }
-       if(flag)
+       if(flag) // if we have found our answer , then don't look
         break;
     }
     dp[index]=flag;
