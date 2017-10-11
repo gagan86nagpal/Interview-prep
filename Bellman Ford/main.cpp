486bf62e10889fe35a22a76545fe8de680cfdccc
gagan86nagpal <gagannagpal68@gmail.com>
2017-10-11 08:53:54 +0530

    Bellman ford for no negative weigth cycles

---
+++ Bellman Ford/main.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+#define inf 10000000
+
+vector <pair <int,int> > graph[1000];
+
+int dist[1000];
+
+void bellman_ford(int s,int v,int e)
+{
+    int i,j,k;
+    for(i=0;i<v;i++)
+        dist[i]=inf;
+    dist[s]=0;
+
+    for(i=0;i<v-1;i++)
+    {
+        for(j=0;j<v;j++)
+        {
+            for(k=0;k<(int)graph[j].size();k++)  // u->v
+            {
+                int u=j;
+                int v=graph[j][k].first;
+                int w=graph[j][k].second;
+                if(dist[v]>dist[u]+w)
+                    dist[v]=dist[u]+w;
+            }
+        }
+    }
+}
+int main()
+{
+    int v,e;
+    int a,b,w;
+    int i;
+    cin>>v>>e;
+    for(i=0;i<e;i++)
+    {
+        cin>>a>>b>>w;
+        graph[a].push_back(make_pair(b,w));
+    }
+    int s;
+    cin>>s;
+    bellman_ford(s,v,e);
+    for(i=0;i<v;i++)
+    {
+        cout<<"distance["<<i<<"]:"<<dist[i]<<"\n";
+    }
+    return 0;
+}
