000ebd446fc435410acbdb22aed189174226a70b
gagan86nagpal <gagannagpal68@gmail.com>
2017-10-27 00:01:28 +0530

    Matrix in Spiral Form

--- Print Matrix in Spiral Form/main.cpp
+++ Print Matrix in Spiral Form/main.cpp
@@ -6,7 +6,7 @@ int mat[200][200];
 void printOuter(int si,int sj,int ei,int ej)
 {
     int r,c;
-    if(si==ei) // one row
+   /* if(si==ei) // one row
     {
         for(c = sj ; c<=ej ;c++)
             cout<<mat[si][c]<<" ";
@@ -18,14 +18,17 @@ void printOuter(int si,int sj,int ei,int ej)
             cout<<mat[r][sj]<<" ";
         return;
     }
+    */
     for(c=sj;c<=ej;c++)
         cout<<mat[si][c]<<" ";
     for(r=si+1;r<=ei;r++)
         cout<<mat[r][ej]<<" ";
-    for(c=ej-1;c>=sj;c--)
-        cout<<mat[ei][c]<<" ";
-    for(r=ei-1;r>si;r--)
-        cout<<mat[r][sj]<<" ";
+    if(si!=ei)   // only 1 row
+        for(c=ej-1;c>=sj;c--)
+            cout<<mat[ei][c]<<" ";
+    if(sj!=ej)   // only 1 column
+        for(r=ei-1;r>si;r--)
+            cout<<mat[r][sj]<<" ";
 }
 void spiral(int n,int m)
 {
@@ -60,8 +63,21 @@ int main()
 9 10 11 12
 13 14 15 16
 
+3 3
+1 2 3
+4 5 6
+7 8 9
+
 3 6
 1 2 3 4 5 6
 7 8 9 10 11 12
 13 14 15 16 17 18
+
+6 3
+1 2 3
+4 5 6
+7 8 9
+10 11 12
+13 14 15
+16 17 18
 */
